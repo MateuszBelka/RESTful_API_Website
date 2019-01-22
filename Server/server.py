9c5c8c409515da7245091272b7f304211c59090c
MateuszBelka <belka.mateusz.bm@gmail.com>
2019-01-22 12:23:42 +0100

    File sorting update #88

---
+++ Server/server.py
@@ -0,0 +1,83 @@
+###############################################################################
+# Web Technology at VU University Amsterdam
+# Assignment 3
+#
+# The assignment description is available on Canvas.
+# This is a template for you to quickly get started with Assignment 3.
+# Read through the code and try to understand it.
+#
+# Have you looked at the documentation of bottle.py?
+# http://bottle.readthedocs.org/en/stable/index.html
+#
+# Once you are familiar with bottle.py and the assignment, start implementing
+# an API according to your design by adding routes.
+###############################################################################
+
+# Include more methods/decorators as you use them
+# See http://bottle.readthedocs.org/en/stable/api.html#bottle.Bottle.route
+
+from bottle import response, error, get
+import json
+
+
+###############################################################################
+# Routes
+#
+# TODO: Add your routes here and remove the example routes once you know how
+#       everything works.
+###############################################################################
+
+@get('/retrieve')
+
+
+@get('/retrieve/<rownumber>')
+
+
+@create('/create')
+
+
+@put('/update/<rownumber>')
+
+
+@delete('/reset')
+
+
+@delete('/reset)
+
+
+
+
+###############################################################################
+# Error handling
+#
+# TODO (optional):
+#       Add sensible error handlers for all errors that may occur when a user
+#       accesses your API.
+###############################################################################
+
+
+###############################################################################
+# This starts the server
+#
+# Access it at http://localhost:8080
+#
+# If you have problems with the reloader (i.e. your server does not
+# automatically reload new code after you save this file), set `reloader=False`
+# and reload manually.
+#
+# You might want to set `debug=True` while developing and/or debugging and to
+# `False` before you submit.
+#
+# The installed plugin 'WtPlugin' takes care of enabling CORS (Cross-Origin
+# Resource Sharing; you need this if you use your API from a website) and
+# provides you with a database cursor.
+###############################################################################
+
+if __name__ == "__main__":
+    from bottle import install, run
+    from wtplugin import WtDbPlugin, WtCorsPlugin
+    
+    install(WtDbPlugin())
+    install(WtCorsPlugin())
+    run(host='localhost', port=8080, reloader=True, debug=True, autojson=False)
+
